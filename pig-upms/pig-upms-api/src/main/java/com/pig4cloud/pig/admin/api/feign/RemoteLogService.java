74156a8d19d71134c0617cccba6fe829049e7a1e
1072718503@qq.com <li1072718503>
2020-03-17 23:19:37 +0800

    项目版本升级,部分代码逻辑修改，由于个人原因项目暂时搁置，如果有人有好的代码。可以进行二次维护

--- pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/feign/RemoteLogService.java
+++ pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/feign/RemoteLogService.java
@@ -1,19 +1,20 @@
 package com.pig4cloud.pig.admin.api.feign;
 
 import com.pig4cloud.pig.admin.api.entity.SysLog;
-import com.pig4cloud.pig.common.core.constant.SecurityConstants;
-import com.pig4cloud.pig.common.core.constant.ServiceNameConstants;
 import com.pig4cloud.pig.common.core.util.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import static com.pig4cloud.pig.common.core.constant.SecurityConstants.FROM;
+import static com.pig4cloud.pig.common.core.constant.ServiceNameConstants.UMPS_SERVICE;
+
 /**
  * @author lengleng
  * @date 2019/2/1
  */
-@FeignClient(contextId = "remoteLogService", value = ServiceNameConstants.UMPS_SERVICE)
+@FeignClient(contextId = "remoteLogService", value = UMPS_SERVICE)
 public interface RemoteLogService {
 	/**
 	 * 保存日志
@@ -23,5 +24,5 @@ public interface RemoteLogService {
 	 * @return succes、false
 	 */
 	@PostMapping("/log")
-	R<Boolean> saveLog(@RequestBody SysLog sysLog, @RequestHeader(SecurityConstants.FROM) String from);
+	R<Boolean> saveLog(@RequestBody SysLog sysLog, @RequestHeader(FROM) String from);
 }
--- pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/feign/RemoteTokenService.java
+++ pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/feign/RemoteTokenService.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+import static com.pig4cloud.pig.common.core.constant.SecurityConstants.FROM;
+import static com.pig4cloud.pig.common.core.constant.ServiceNameConstants.AUTH_SERVICE;
+
 /**
  * @author lengleng
  * @date 2019/2/1
  */
-@FeignClient(contextId = "remoteTokenService", value = ServiceNameConstants.AUTH_SERVICE)
+@FeignClient(contextId = "remoteTokenService", value = AUTH_SERVICE)
 public interface RemoteTokenService {
 	/**
 	 * 分页查询token 信息
@@ -22,7 +25,7 @@ public interface RemoteTokenService {
 	 * @return page
 	 */
 	@PostMapping("/token/page")
-	R getTokenPage(@RequestBody Map<String, Object> params, @RequestHeader(SecurityConstants.FROM) String from);
+	R getTokenPage(@RequestBody Map<String, Object> params, @RequestHeader(FROM) String from);
 
 	/**
 	 * 删除token
@@ -32,5 +35,5 @@ public interface RemoteTokenService {
 	 * @return
 	 */
 	@DeleteMapping("/token/{token}")
-	R<Boolean> removeToken(@PathVariable("token") String token, @RequestHeader(SecurityConstants.FROM) String from);
+	R<Boolean> removeToken(@PathVariable("token") String token, @RequestHeader(FROM) String from);
 }
